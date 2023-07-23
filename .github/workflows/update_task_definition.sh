29236303e50bfef15e804128d99a3a774236cb66
Ziqi Wang <ziqi.wang39@gmail.com>
2023-07-23 17:54:51 +1000

    Update variable name

--- .github/workflows/update_task_definition.sh
+++ .github/workflows/update_task_definition.sh
@@ -5,7 +5,7 @@ GITHUB_SHA=${GITHUB_SHA:0:7}
 ENVIRONMENT=${TARGET_ENV}
 
 # Update the image version in the task-definition.json file
-sed -i 's~349498089405.dkr.ecr.ap-southeast-2.amazonaws.com/crankbit-backend-'"$ENVIRONMENT"':latest~349498089405.dkr.ecr.ap-southeast-2.amazonaws.com/crankbit-backend-'"$ENVIRONMENT"':'"$GITHUB_SHA"'~g' .github/workflows/task_definition.json
+sed -i 's/349498089405.dkr.ecr.ap-southeast-2.amazonaws.com\/crankbit-backend-main:latest/349498089405.dkr.ecr.ap-southeast-2.amazonaws.com\/crankbit-backend-'"$ENVIRONMENT"':'"$GITHUB_SHA"'/g' .github/workflows/task-definition.json
 
 # Update the family and executionRoleArn values in the task-definition.json file
 sed -i 's/crankbit_backend_task_.*\([^"]*\)/crankbit_backend_task_'"$ENVIRONMENT"'\1/' .github/workflows/task_definition.json
