278c579138ba58906f0553a692ca6ee8563ef7df
Ziqi Wang <ziqi.wang39@gmail.com>
2023-07-23 18:29:25 +1000

    Amend script

--- .github/workflows/update_task_definition.sh
+++ .github/workflows/update_task_definition.sh
@@ -8,8 +8,8 @@ ENVIRONMENT=${TARGET_ENV}
 sed -i 's/349498089405.dkr.ecr.ap-southeast-2.amazonaws.com\/crankbit-backend-main:latest/349498089405.dkr.ecr.ap-southeast-2.amazonaws.com\/crankbit-backend-'"$ENVIRONMENT"':'"$GITHUB_SHA"'/g' .github/workflows/task_definition.json
 
 # Update the family and executionRoleArn values in the task-definition.json file
-sed -i 's/crankbit_backend_task_.*\([^"]*\)/crankbit_backend_task_'"$ENVIRONMENT"'\1/' .github/workflows/task_definition.json
-sed -i 's/ecs-task-execution-role-.*\([^"]*\)/ecs-task-execution-role-'"$ENVIRONMENT"'\1/' .github/workflows/task_definition.json
+sed -i 's/"family": "crankbit_backend_task_main"/"family": "crankbit_backend_task_'"$ENVIRONMENT"'"/' .github/workflows/task_definition.json
+sed -i 's/"executionRoleArn": "arn:aws:iam::349498089405:role\/ecs-task-execution-role-main"/"executionRoleArn": "arn:aws:iam::349498089405:role\/ecs-task-execution-role-'"$ENVIRONMENT"'"/' .github/workflows/task_definition.json
 
 # Update the container name in the task-definition.json file (containerDefinitions block)
 sed -i 's/"name": "crankbit-backend-.*"/"name": "crankbit-backend-'"$ENVIRONMENT"'"/' .github/workflows/task_definition.json
