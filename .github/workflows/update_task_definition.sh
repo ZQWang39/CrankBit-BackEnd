35df5e230ef6e5e69eedd60598460afebef3efb8
Ziqi Wang <ziqi.wang39@gmail.com>
2023-07-23 17:09:30 +1000

    Update script path

--- .github/workflows/update_task_definition.sh
+++ .github/workflows/update_task_definition.sh
@@ -5,4 +5,4 @@ GITHUB_SHA=${GITHUB_SHA:0:7}
 # ENVIRONMENT=${ENVIRONMENT}
 
 # Update the image version in the task-definition.json file
-sed -i 's/349498089405.dkr.ecr.ap-southeast-2.amazonaws.com\/crankbit-backend-main:latest/349498089405.dkr.ecr.ap-southeast-2.amazonaws.com\/crankbit-backend-main:'"$GITHUB_SHA"'/g' .github/workflows/task-definition.json
+sed -i 's/349498089405.dkr.ecr.ap-southeast-2.amazonaws.com\/crankbit-backend-main:latest/349498089405.dkr.ecr.ap-southeast-2.amazonaws.com\/crankbit-backend-main:'"$GITHUB_SHA"'/g' .github/workflows/task_definition_main.json
