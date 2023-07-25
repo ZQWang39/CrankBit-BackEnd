212b70f1a5297485f4a2c99295b0895cc8718a1e
Ziqi Wang <ziqi.wang39@gmail.com>
2023-07-25 11:50:20 +1000

    Add awslogs to task_definition.json

--- .github/workflows/update_task_definition.sh
+++ .github/workflows/update_task_definition.sh
@@ -10,6 +10,9 @@ sed -i 's/349498089405.dkr.ecr.ap-southeast-2.amazonaws.com\/crankbit-backend-ma
 # Update  container name in the task-definition.json file (containerDefinitions block)
 sed -i 's/"name": "crankbit-backend-.*"/"name": "crankbit-backend-'"$ENVIRONMENT"'"/' .github/workflows/task_definition.json
 
+#Update logConfiguration awslogs-group value in the task-definition.json file
+sed -i 's/"awslogs-group": "/ecs/awslogs/Crankbit-cluster-main"/"awslogs-group": "/ecs/awslogs/Crankbit-cluster-'"$ENVIRONMENT"'"/' .github/workflows/task_definition.json
+
 # Update family value in the task-definition.json file
 sed -i 's/"family": "crankbit_backend_task_main"/"family": "crankbit_backend_task_'"$ENVIRONMENT"'"/' .github/workflows/task_definition.json
 
